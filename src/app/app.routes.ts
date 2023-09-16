aeaf96f6927ff3d0859170702654ac3938592b02
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-09-16 16:50:08 -0300

    Adicionado configuracao de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from "./navegacao/home/home.component";
 import { ContatoComponent } from "./institucional/contato/contato.component";
 import { SobreComponent } from "./institucional/sobre/sobre.component";
 
-export const rootRouteConfig: Routes = [
+export const rootRouterConfig: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'contato', component: ContatoComponent },
